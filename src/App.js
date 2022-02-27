4532a567c0f7954c3cbc6514c9c3d4c08f106b41
JDS <jeongdeoksu@gmail.com>
2022-02-27 16:30:51 +0900

    add blog navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  let [글제목, 글제목변경] = useState(['강남 냉면 맛집', '종로 냉면 맛집', '의정부 냉면 맛집']);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>개발 Blog</div>
+      <div className='list'>
+        <h3>{ 글제목[0] }</h3>
+        <p>2월 17일</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h3>{ 글제목[1] }</h3>
+        <p>2월 17일</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h3>{ 글제목[2] }</h3>
+        <p>2월 17일</p>
+        <hr/>
+      </div>
     </div>
   );
 }
